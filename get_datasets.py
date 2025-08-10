c9a27f068e4fd01008045c21f227e44f684636da
tim_legge <tlegge@ucsd.edu>
2025-08-09 23:58:28 -0700

    updated get_datasets

--- get_datasets.py
+++ get_datasets.py
@@ -38,13 +38,13 @@ datasets = {
         ],
     },
     'HLS4ML': {
-        './': [
+        '../train': [
             ('https://zenodo.org/records/3602260/files/hls4ml_LHCjet_150p_train.tar.gz', 'af12d3a500414924e70a73b56fc49aec'),
         ],
-        './': [
+        '../test': [
             ('https://zenodo.org/records/3602260/files/hls4ml_LHCjet_150p_train.tar.gz', 'af12d3a500414924e70a73b56fc49aec'),
         ],
-        './': [
+        '../val': [
             ('https://zenodo.org/records/3602260/files/hls4ml_LHCjet_150p_val.tar.gz', 'ff902e6a02bfb4247a9068bf7ff7f6db'),
         ],
     },
