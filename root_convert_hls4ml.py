cc7c36a5310f54ae21db5f0d878777cefdb89e35
tim_legge <tlegge@ucsd.edu>
2025-08-16 17:21:54 -0700

    update root conversion again

--- root_convert_hls4ml.py
+++ root_convert_hls4ml.py
@@ -7,14 +7,17 @@ if not os.path.exists('datasets/hls4ml/HLS4ML/val/val-root'):
 if not os.path.exists('datasets/hls4ml/HLS4ML/train/train-root'):
     os.makedirs('datasets/hls4ml/HLS4ML/train/train-root')
 
-for f in os.listdir('datasets/hls4ml/HLS4ML/val/val-parquet'):
+train_parquet_dir = 'datasets/hls4ml/HLS4ML/train/train-parquet'
+val_parquet_dir = 'datasets/hls4ml/HLS4ML/val/val-parquet'
+
+for f in os.listdir(val_parquet_dir):
     if f.endswith('.parquet'):
         print(f'Converting {f} to ROOT format...')
         parquet_to_root('datasets/hls4ml/HLS4ML/val/val-root/' + os.path.split(os.path.splitext(f)[0])[-1] + '.root',
-                            f, name='tree', force=True)
+                            os.path.join(val_parquet_dir, f), name='tree', force=True)
 
-for f in os.listdir('datasets/hls4ml/HLS4ML/train/train-parquet'):
+for f in os.listdir(train_parquet_dir):
     if f.endswith('.parquet'):
         print(f'Converting {f} to ROOT format...')
         parquet_to_root('datasets/hls4ml/HLS4ML/train/train-root/' + os.path.split(os.path.splitext(f)[0])[-1] + '.root',
-                            f, name='tree', force=True)
+                            os.path.join(train_parquet_dir, f), name='tree', force=True)
