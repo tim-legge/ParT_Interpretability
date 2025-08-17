661761ffab3337a7fecd57b19cce53697972ddbe
tim_legge <tlegge@ucsd.edu>
2025-08-16 17:17:47 -0700

    update root conversion

--- root_convert_hls4ml.py
+++ root_convert_hls4ml.py
@@ -10,11 +10,11 @@ if not os.path.exists('datasets/hls4ml/HLS4ML/train/train-root'):
 for f in os.listdir('datasets/hls4ml/HLS4ML/val/val-parquet'):
     if f.endswith('.parquet'):
         print(f'Converting {f} to ROOT format...')
-        parquet_to_root('datasets/hls4ml/HLS4ML/val/val-root/' + f.split('/')[-1].split('.p')[0] + '.root',
+        parquet_to_root('datasets/hls4ml/HLS4ML/val/val-root/' + os.path.split(os.path.splitext(f)[0])[-1] + '.root',
                             f, name='tree', force=True)
 
 for f in os.listdir('datasets/hls4ml/HLS4ML/train/train-parquet'):
     if f.endswith('.parquet'):
         print(f'Converting {f} to ROOT format...')
-        parquet_to_root('datasets/hls4ml/HLS4ML/train/train-root/' + f.split('/')[-1].split('.p')[0] + '.root',
+        parquet_to_root('datasets/hls4ml/HLS4ML/train/train-root/' + os.path.split(os.path.splitext(f)[0])[-1] + '.root',
                             f, name='tree', force=True)
