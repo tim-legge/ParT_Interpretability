473c3e14f8499d1a3a685315da04ee12d24c3db6
tim_legge <tlegge@ucsd.edu>
2025-08-27 19:36:50 -0700

    updated dataloading

--- dataloading_full.py
+++ dataloading_full.py
@@ -5,6 +5,6 @@ parquet_to_root("./datasets/qg_dataset/QuarkGluon/qg_test_file_0.root", "./datas
 parquet_to_root("./datasets/qg_dataset/QuarkGluon/qg_test_file_1.root", "./datasets/qg_dataset/QuarkGluon/test_file_1.parquet", name="tree", force=True)
 
 merge_root("/datasets/qg_dataset/QuarkGluon/qg_test_file.root",
-           ["./datasets/qg_dataset/QuarkGluon/qg_test_file_0.root", "./datasets/qg_dataset/QuarkGluon/qg_test_file_1.root"], name="tree", force=True)
+           ["./datasets/qg_dataset/QuarkGluon/qg_test_file_0.root", "./datasets/qg_dataset/QuarkGluon/qg_test_file_1.root"], force=True)
 
 parquet_to_root("./datasets/tl_dataset/TopLandscape/tl_test_file.root", "./datasets/tl_dataset/TopLandscape/test_file.parquet", name="tree", force=True)
