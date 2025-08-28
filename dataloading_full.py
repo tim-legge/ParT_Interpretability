07408b79e6a173ed207538d03638dd3f2067a092
tim_legge <tlegge@ucsd.edu>
2025-08-27 19:28:00 -0700

    scripts for pod

--- dataloading_full.py
+++ dataloading_full.py
@@ -1,9 +1,10 @@
-from hepconvert import parquet_to_root
+from hepconvert import parquet_to_root, merge_root
 import os
 
-for i, f in enumerate(os.listdir("./datasets/qg_dataset/QuarkGluon/")):
-    if f.startswith("test") and f.endswith(".parquet"):
-        qg_datapath = os.path.join('./datasets/qg_dataset/QuarkGluon/', f'{f.replace('.parquet', '.root')}')
-    parquet_to_root(f"./datasets/qg_dataset/QuarkGluon/qg_test_file_{i}.root", qg_datapath, name="tree", force=True)
+parquet_to_root("./datasets/qg_dataset/QuarkGluon/qg_test_file_0.root", "./datasets/qg_dataset/QuarkGluon/test_file_0.parquet", name="tree", force=True)
+parquet_to_root("./datasets/qg_dataset/QuarkGluon/qg_test_file_1.root", "./datasets/qg_dataset/QuarkGluon/test_file_1.parquet", name="tree", force=True)
+
+merge_root("/datasets/qg_dataset/QuarkGluon/qg_test_file.root",
+           ["./datasets/qg_dataset/QuarkGluon/qg_test_file_0.root", "./datasets/qg_dataset/QuarkGluon/qg_test_file_1.root"], name="tree", force=True)
 
 parquet_to_root("./datasets/tl_dataset/TopLandscape/tl_test_file.root", "./datasets/tl_dataset/TopLandscape/test_file.parquet", name="tree", force=True)
