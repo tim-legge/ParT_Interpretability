b09bcb5eaf87ec0f0fba08ee7e768f6f7b9d3cbb
tim_legge <tlegge@ucsd.edu>
2025-08-27 21:28:30 -0700

    sfbfdnd

--- Modded_QG_and_TL_AttentionGraph_pt1.py
+++ Modded_QG_and_TL_AttentionGraph_pt1.py
@@ -1162,11 +1162,11 @@ def load_data(dataset_type='qg', batch_size=300):
                     #print(f'label shapes before concat: {data["labels"].shape}, {data_1["labels"].shape}')
                     # Truncate to batch_size            
                     #print(f"Truncating from {data['pf_points'].shape[0]} jets to {batch_size} jets")
-                    data['pf_points'] = np.concatenate(data['pf_points'][:],data_1['pf_points'][:])
-                    data['pf_features'] = np.concatenate(data['pf_features'][:],data_1['pf_features'][:])
-                    data['pf_vectors'] = np.concatenate(data['pf_vectors'][:],data_1['pf_vectors'][:])
-                    data['pf_mask'] = np.concatenate(data['pf_mask'][:],data_1['pf_mask'][:])
-                    data['labels'] = np.concatenate(data['labels'][:],data_1['labels'][:])                
+                    data['pf_points'] = np.concatenate((data['pf_points'][:],data_1['pf_points'][:]))
+                    data['pf_features'] = np.concatenate((data['pf_features'][:],data_1['pf_features'][:]))
+                    data['pf_vectors'] = np.concatenate((data['pf_vectors'][:],data_1['pf_vectors'][:]))
+                    data['pf_mask'] = np.concatenate((data['pf_mask'][:],data_1['pf_mask'][:]))
+                    data['labels'] = np.concatenate((data['labels'][:],data_1['label'][:]))                
             print('QG collected')
             return data
 
