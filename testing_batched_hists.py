eca6525a0f9facab43d2380f37f4f5b2a5e0314b
tim_legge <tlegge@ucsd.edu>
2025-08-29 01:05:46 -0700

    bgeg

--- testing_batched_hists.py
+++ testing_batched_hists.py
@@ -1,5 +1,8 @@
 import numpy as np
 import os
 
-array = np.load('/part-vol-3/timlegge-ParT-trained/batched_hists/qg_attention_distribution_batch_0.npy', allow_pickle=True)
+#array = np.load('/part-vol-3/timlegge-ParT-trained/batched_hists/qg_attention_distribution_batch_0.npy', allow_pickle=True)
+#print(array)
+
+array = np.load('/part-vol-3/timlegge-ParT-trained/batched_hists/qg_attention_distribution_batch_1.npy', allow_pickle=True)
 print(array)
