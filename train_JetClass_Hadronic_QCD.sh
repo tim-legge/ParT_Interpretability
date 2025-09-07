615759139902ac8400319f07e669c5dda09fddca
tim_legge <tlegge@ucsd.edu>
2025-09-07 12:31:05 -0700

    different training

--- train_JetClass_Hadronic_QCD.sh
+++ train_JetClass_Hadronic_QCD.sh
@@ -59,8 +59,8 @@ fi
 SAMPLE_TYPE=Pythia
 
 $CMD \
-    --data-train "TTBar:/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/train_100M/TTBar_000.root" \
-    "ZJetsToNuNu:/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/train_100M/ZJetsToNuNu_000.root" \
+    --data-train "TTBar:/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/train_100M/TTBar_00[0-1].root" \
+    "ZJetsToNuNu:/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/train_100M/ZJetsToNuNu_00[0-1].root" \
     --data-val "TTBar:/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/val_5M/TTBar_120.root" \
     "ZJetsToNuNu:/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/val_5M/ZJetsToNuNu_120.root" \
     --data-test \
