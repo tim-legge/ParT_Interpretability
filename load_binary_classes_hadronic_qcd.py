1c294388f0fd38ea09314a9f7e5e4cbcf8ac2e3c
tim_legge <tlegge@ucsd.edu>
2025-09-08 15:28:36 -0700

    new files

--- load_binary_classes_hadronic_qcd.py
+++ load_binary_classes_hadronic_qcd.py
@@ -847,7 +847,7 @@ print('Downloaded full JC dataset!')
 
 
 
-np.save('./jck_hadronic_qcd_pf_points', jck_hadronic_qcd_data['pf_points'])
+np.save('./jc_hadronic_qcd_pf_points', jck_hadronic_qcd_data['pf_points'])
 np.save('./jc_hadornic_qcd_pf_features', jck_hadronic_qcd_data['pf_features'])
 np.save('./jc_hadronic_qcd_pf_vectors', jck_hadronic_qcd_data['pf_vectors'])
 np.save('./jc_hadronic_qcd_pf_mask', jck_hadronic_qcd_data['pf_mask'])
