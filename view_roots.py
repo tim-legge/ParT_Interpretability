73f9875bd50bc4303500d39acb82835091617479
tim_legge <tlegge@ucsd.edu>
2025-09-10 20:50:31 -0700

    underscorre

--- view_roots.py
+++ view_roots.py
@@ -1,7 +1,7 @@
 import uproot
 import numpy as np
 
-with uproot.open("/part-vol-3/weaver-core/particle-transformer/datasets/JetClass/Pythia/train_100M/TTBar_000.root") as file:
+with uproot.open("/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/train_100M/TTBar_000.root") as file:
     tree = file["tree"]
     features = tree.arrays(filter_name="*part_*")
     if "part_d0" in features:
