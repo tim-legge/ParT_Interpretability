27a14904ad61bd92afe8f8429018dfbe143b4c90
tim_legge <tlegge@ucsd.edu>
2025-09-10 20:53:00 -0700

    more print statements

--- view_roots.py
+++ view_roots.py
@@ -1,8 +1,12 @@
 import uproot
 import numpy as np
 
+print("Checking for part_d0 in the dataset...")
+
 with uproot.open("/part-vol-3/weaver-core/particle_transformer/datasets/JetClass/Pythia/train_100M/TTBar_000.root") as file:
     tree = file["tree"]
+    print("Available branches:", tree.keys())
+    print("Filtering branches now...")
     features = tree.arrays(filter_name="*part_*")
     if "part_d0" in features:
         part_d0 = features["part_d0"].to_numpy()
