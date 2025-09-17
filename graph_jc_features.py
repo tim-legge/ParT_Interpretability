47b78877220a8f1d989e5b63291c0fda4427dc02
tim_legge <tlegge@ucsd.edu>
2025-09-17 13:30:30 -0700

    try to actually get hists now

--- graph_jc_features.py
+++ graph_jc_features.py
@@ -15,50 +15,97 @@ def mask_out(feats, vectors, mask):
     
     return masked_feats, masked_vecs
 
-def sort_feats(masked_feats, masked_vecs):
+def sort_feats(masked_feats, masked_vecs, feats_dict=None, vecs_dict=None):
 
-    feats_dict = {
-        'part_pt_log': [],
-        'part_e_log': [],
-        'part_log_ptrel': [],
-        'part_log_erel': [],
-        'part_deltaR': [],
-        'part_charge': [],
-        'part_isChargedHadron': [],
-        'part_isNeutralHadron': [],
-        'part_isPhoton': [],
-        'part_isElectron': [],
-        'part_isMuon': [],
-        'part_d0': [],
-        'part_d0err': [],
-        'part_dz': [],
-        'part_dzerr': [],
-        'part_deta': [],
-    }
-
-    for jet in masked_feats:
+    if feats_dict is None:
+        feats_dict = {
+            'part_pt_log': [],
+            'part_e_log': [],
+            'part_log_ptrel': [],
+            'part_log_erel': [],
+            'part_deltaR': [],
+            'part_charge': [],
+            'part_isChargedHadron': [],
+            'part_isNeutralHadron': [],
+            'part_isPhoton': [],
+            'part_isElectron': [],
+            'part_isMuon': [],
+            'part_d0': [],
+            'part_d0err': [],
+            'part_dz': [],
+            'part_dzerr': [],
+            'part_deta': [],
+        }
+    else:
+        feats_dict = feats_dict
+    
+    for jet_idx, jet in enumerate(masked_feats):
         for idx, key in enumerate(feats_dict.keys()):
-            feats_dict[key].extend(jet[idx])
-        
-    vecs_dict = {
-        'part_px': [],
-        'part_py': [],
-        'part_pz': [],
-        'part_energy': [],
-    }
+        #if idx >= 13 and np.random.rand() < 0.01:
+        #    print(f'jet_idx: {jet_idx}, idx: {idx}, key: {key}')
+            try:
+                feats_dict[key].extend(masked_feats[jet_idx][idx].flatten().tolist())
+            # we need to check that padding is actually being removed
+            #if np.random.rand() < 0.01:
+            #    print(f'Num of particles: {len(masked_feats[jet_idx][idx])} for jet_idx {jet_idx}, idx {idx}, key {key}')
+            except IndexError as e:
+                print(f"IndexError for jet_idx {jet_idx}, idx {idx}, key {key}: {e}")
+                continue
+    if vecs_dict is None:    
+        vecs_dict = {
+            'part_px': [],
+            'part_py': [],
+            'part_pz': [],
+            'part_energy': [],
+        }
+    else:
+        vecs_dict = vecs_dict
 
-    for jet in masked_vecs:
+    for jet_idx, jet in enumerate(masked_vecs):
         for idx, key in enumerate(vecs_dict.keys()):
-            vecs_dict[key].extend(jet[idx])
+            try:
+                vecs_dict[key].extend(masked_feats[jet_idx][idx].flatten().tolist())
+            except IndexError as e:
+                print(f"IndexError for jet_idx {jet_idx}, idx {idx}, key {key}: {e}")
+                continue
 
     return feats_dict, vecs_dict
 
-def compile_histograms(data_dir, output_dir):
+def compile_histograms(data_dir, output_dir, feats_dict=None, vecs_dict=None, labelstype='jc'):
     assert os.path.exists(data_dir), f"Data directory {data_dir} does not exist."
     assert os.path.exists(output_dir), f"Output directory {output_dir} does not exist."
     print(f"Processing data from {data_dir} and saving histograms to {output_dir}")
+    if labelstype == 'jc' or None:
+        labels_list = ['QCD', 'Hbb', 'Hcc', 'Hgg', 'H4q', 'Hqql', 'Zqq', 'Wqq', 'Tbqq', 'Tbl']
+    elif labelstype == 'tl':
+        labels_list = ['QCD', 'Tbqq']
 
-    labels_list = ['QCD', 'Hbb', 'Hcc', 'Hgg', 'H4q', 'Hqql', 'Zqq', 'Wqq', 'Tbqq', 'Tbl']
+    feat_ranges = {
+        'part_pt_log': (-2.4, 6.3),
+        'part_e_log': (-0.9, 7.0),
+        'part_log_ptrel': (-8.6, -0.1),
+        'part_log_erel': (-7.7, -0.2),
+        'part_deltaR': (0, 3.0),
+        'part_charge': (-1.0, 1.0),
+        'part_isChargedHadron': (0.0, 1.0),
+        'part_isNeutralHadron': (0.0, 1.0),
+        'part_isPhoton': (0.0, 1.0),
+        'part_isElectron': (0.0, 1.0),
+        'part_isMuon': (0.0, 1.0),
+        'part_d0': (-1.0, 1.0),
+        'part_d0err': (0.0, 0.7),
+        'part_dz': (-1.0, 1.0),
+        'part_dzerr': (0.0, 2.2),
+        'part_deta': (-2.9, 2.5),
+        'part_dphi': (-0.9, 0.9),
+    }
+
+    vec_ranges = {
+        'part_px': (-2.4, 6.3),
+        'part_py': (-0.9, 7.0),
+        'part_pz': (-8.7, 0.2),
+        'part_energy': (-7.7, -0.2),
+    }
 
     # look at the counter in the output dir
     if not os.path.exists(os.path.join(output_dir, 'hist_counter.txt')):
@@ -77,23 +124,30 @@ def compile_histograms(data_dir, output_dir):
             vecs = np.load(os.path.join(data_dir, vec_file))
 
             # determine label
-            if np.argmax(labels[0], axis=1) == np.argmax(labels[-1], axis=1):
-                label = np.argmax(labels[0], axis=1)
-                label_name = labels_list[label]
-            else:
-                print("Warning: Inconsistent labels in the batch, skipping this batch.")
-                continue
-
+            if labelstype == 'jc':
+                if np.argmax(labels[0], axis=1) == np.argmax(labels[-1], axis=1):
+                    label = np.argmax(labels[0], axis=1)
+                    label_name = labels_list[label]
+                else:
+                    print("Warning: Inconsistent labels in the batch, skipping this batch.")
+                    continue
+            elif labelstype == 'tl':
+                if labels[0] == labels[-1]:
+                    label = labels[0]
+                    label_name = labels_list[label]
+                else:
+                    print("Warning: Inconsistent labels in the batch, skipping this batch.")
+                    continue
             masked_feats, masked_vecs = mask_out(feats, vecs, masks)
-            feats_dict, vecs_dict = sort_feats(masked_feats, masked_vecs)
+            feats_dict, vecs_dict = sort_feats(masked_feats, masked_vecs, feats_dict=feats_dict, vecs_dict=vecs_dict)
 
             if feats_hists is None:
                 feats_hists = [np.zeros(50) for key in feats_dict.keys()]
                 vecs_hists = [np.zeros(50) for key in vecs_dict.keys()]
 
             # TODO: Figure out the right ranges for each feature (use 100k example jets probably)
-            feats_hists += [np.histogram(feats_dict[key], bins=50) for key in feats_dict.keys()]
-            vecs_hists += [np.histogram(vecs_dict[key], bins=50) for key in vecs_dict.keys()]
+            feats_hists += [np.histogram(feats_dict[key], bins=50, range=feat_ranges[key]) for key in feats_dict.keys()]
+            vecs_hists += [np.histogram(vecs_dict[key], bins=50, range=vec_ranges[key]) for key in vecs_dict.keys()]
         print(f"Completed processing batch {hist_counter}, saving histograms.")
         for idx, key in enumerate(feats_dict.keys()):
             np.save(os.path.join(output_dir, f"{label_name}_hist_{key}.npy"), feats_hists[idx])
--- graph_tl_features.py
+++ graph_tl_features.py
@@ -2,10 +2,16 @@
 
 import os
 import numpy as np
-import matplotlib
-from graph_jc_features import mask_out, compile_histograms
+import matplotlib.pyplot as plt
+from graph_jc_features import mask_out, sort_feats, compile_histograms
 
-def sort_feats(masked_feats, masked_vecs):
+if __name__ == "__main__":
+
+    features_dir = '/part-vol-3/timlegge-ParT-trained/data_from_tl_train/'
+    output_dir = '/part-vol-3/timlegge-ParT-trained/histograms_tl_feats/'
+    assert os.path.exists(features_dir), f"Features directory {features_dir} does not exist."
+    assert os.path.exists(output_dir), f"Output directory {output_dir} does not exist."
+    print(f"Reading features from {features_dir} and saving histograms to {output_dir}")
 
     feats_dict = {
         'part_pt_log': [],
@@ -17,34 +23,9 @@ def sort_feats(masked_feats, masked_vecs):
         'part_dphi': [],
     }
 
-    for jet in masked_feats:
-        for idx, key in enumerate(feats_dict.keys()):
-            feats_dict[key].extend(jet[idx])
-        
-    vecs_dict = {
-        'part_px': [],
-        'part_py': [],
-        'part_pz': [],
-        'part_energy': [],
-    }
-
-    for jet in masked_vecs:
-        for idx, key in enumerate(vecs_dict.keys()):
-            vecs_dict[key].extend(jet[idx])
-
-    return feats_dict, vecs_dict
-
-if __name__ == "__main__":
-
-    features_dir = '/part-vol-3/timlegge-ParT-trained/data_from_tl_train/'
-    output_dir = '/part-vol-3/timlegge-ParT-trained/histograms_tl_feats/'
-    assert os.path.exists(features_dir), f"Features directory {features_dir} does not exist."
-    assert os.path.exists(output_dir), f"Output directory {output_dir} does not exist."
-    print(f"Reading features from {features_dir} and saving histograms to {output_dir}")
-
     feature_files = [f for f in sorted(os.listdir(features_dir)) if 'pf_features' in f and f.endswith('.npy')]
     mask_files = [f for f in sorted(os.listdir(features_dir)) if 'pf_mask' in f and f.endswith('.npy')]
     label_files = [f for f in sorted(os.listdir(features_dir)) if 'labels' in f and f.endswith('.npy')]
     vector_files = [f for f in sorted(os.listdir(features_dir)) if 'pf_vectors' in f and f.endswith('.npy')]
 
-    compile_histograms(data_dir=features_dir, output_dir=output_dir)
+    compile_histograms(data_dir=features_dir, output_dir=output_dir, feats_dict=feats_dict, vecs_dict=None)
