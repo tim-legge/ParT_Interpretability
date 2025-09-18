c83fc020635f748183ab9bbdc2d06de580366b89
tim_legge <tlegge@ucsd.edu>
2025-09-17 23:29:37 -0700

    try the features getting thing

--- graph_jc_features.py
+++ graph_jc_features.py
@@ -13,6 +13,8 @@ def mask_out(feats, vectors, mask):
     for jet_idx, jet in enumerate(vectors):
         masked_vecs.append(jet[:, :np.sum(mask[jet_idx].astype('bool'))])
     
+    print(masked_feats[0][0])
+    
     return masked_feats, masked_vecs
 
 def sort_feats(masked_feats, masked_vecs, feats_dict=None, vecs_dict=None):
