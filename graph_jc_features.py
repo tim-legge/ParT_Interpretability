89278a38963c91ad09f7d37a8d3fbc3a0ad31b67
tim_legge <tlegge@ucsd.edu>
2025-09-17 23:49:22 -0700

    defnjyr

--- graph_jc_features.py
+++ graph_jc_features.py
@@ -47,7 +47,7 @@ def sort_feats(masked_feats, masked_vecs, feats_dict=None, vecs_dict=None):
         #    print(f'jet_idx: {jet_idx}, idx: {idx}, key: {key}')
             try:
                 feats_dict[key].extend(masked_feats[jet_idx][idx].flatten().tolist())
-                assert sum(masked_feats[jet_idx][idx]) != 0.0, "Feature appears to be all zeros, something is wrong"
+                assert sum(masked_feats[jet_idx][idx]) != 0.0, f"Feature appears to be all zeros, something is wrong: {masked_feats[jet_idx][idx][:15]}"
             # we need to check that padding is actually being removed
             #if np.random.rand() < 0.01:
             #    print(f'Num of particles: {len(masked_feats[jet_idx][idx])} for jet_idx {jet_idx}, idx {idx}, key {key}')
