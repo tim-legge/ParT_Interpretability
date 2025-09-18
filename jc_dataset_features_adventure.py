0fa44953654839fa6e3a3f5f56dcea6bbef70a40
tim_legge <tlegge@ucsd.edu>
2025-09-17 23:17:54 -0700

    etrh

--- jc_dataset_features_adventure.py
+++ jc_dataset_features_adventure.py
@@ -1,6 +1,6 @@
 import os
 import numpy as np
 
-array = np.load('/part-vol-3/timlegge-ParT-trained/data_from_jc_train/pf_features_0.npy')
+array = np.load('/part-vol-3/timlegge-ParT-trained/histograms_jc_feats/Tbqq_27_hist_part_log_ptrel.npy')
 
 print(array[0])
